6f4245dc1c3f1e53ccda8f9e739eecf03833fcf4
Francisco Rubial <franjorub@gmail.com>
2019-12-17 15:46:26 -0400

    added pages structure and include into App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { AuthContext } from "./contexts/auth";
+import { PrivateRoute } from "./components/PrivateRoute";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <AuthContext.Provider value={false}>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Login Page</Link>
+            </li>
+            <li>
+              <Link to="/welcome">Home Page</Link>
+            </li>
+          </ul>
+          <Route exact path="/" component={Login} />
+          <PrivateRoute path="/welcome" component={Home} />
+        </div>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
