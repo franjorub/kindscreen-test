288df4b6d27cb53fd580d3b779cad22f56b548c1
Francisco Rubial <franjorub@gmail.com>
2019-12-17 19:56:47 -0400

    removed links at top added local storage session

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-import React from "react";
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 import { AuthContext } from "./contexts/auth";
 import { PrivateRoute } from "./components/PrivateRoute";
 
-function App() {
+const App = () => {
+  const [loggedUser, setLoggedUser] = useState(null);
+
+  useEffect(() => {
+    const checkLocalSession = async () => {
+      const localUser = localStorage.getItem("user");
+      if (localUser) {
+        const user = await JSON.parse(localUser);
+        setLoggedUser(user);
+      }
+    };
+
+    checkLocalSession();
+  }, []);
+
+  const authUser = user => {
+    localStorage.setItem("user", JSON.stringify(user));
+    setLoggedUser(user);
+  };
+
   return (
-    <AuthContext.Provider value={false}>
+    <AuthContext.Provider value={{ loggedUser, authUser }}>
       <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/">Login Page</Link>
-            </li>
-            <li>
-              <Link to="/welcome">Home Page</Link>
-            </li>
-          </ul>
-          <Route exact path="/" component={Login} />
-          <PrivateRoute path="/welcome" component={Home} />
-        </div>
+        <Route exact path="/" component={Login} />
+        <PrivateRoute path="/welcome" component={Home} />
       </Router>
     </AuthContext.Provider>
   );
-}
+};
 
 export default App;
