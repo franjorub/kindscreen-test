eadfabcb41ac0b4c749c60e691bc2e58a5a1666e
Francisco Rubial <franjorub@gmail.com>
2019-12-17 19:59:51 -0400

    get and print logged user

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,3 +1,12 @@
 import React from "react";
+import { useAuth } from "../../contexts/auth";
 
-export const Home = () => <div>Home</div>;
+export const Home = () => {
+  const { loggedUser } = useAuth();
+
+  return (
+    <div>
+      <h1>Welcome: {loggedUser.name}</h1>
+    </div>
+  );
+};
