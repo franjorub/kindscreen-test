9062a1d7d6f8ce2402b81d06e50d3bce52065b69
Francisco Rubial <franjorub@gmail.com>
2019-12-18 00:13:10 -0400

    display post data

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,20 +1,48 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Row, Col, Container } from "reactstrap";
+import { Row, Col, Container, Button } from "reactstrap";
 import styled from "styled-components";
+import { usePosts } from "../contexts/posts";
+import { starredPost, unStarredPost } from "../actions/posts";
 
 const CustomContainer = styled(Container)`
   margin-bottom: 12px;
 `;
 
-export const Post = ({ post }) => (
-  <CustomContainer key={post.id}>
-    <Row>
-      <Col>
-        <h3>{post.title}</h3>
-        <p>{post.body}</p>
-        <Link to={`/post/${post.id}/comments`}>View comments</Link>
-      </Col>
-    </Row>
-  </CustomContainer>
-);
+export const Post = ({ id, showStarred }) => {
+  const [posts, dispatchToPosts] = usePosts();
+
+  const post = { ...posts.entities[id] };
+
+  const handleStarred = () => {
+    if (post.isStarred) {
+      dispatchToPosts(unStarredPost(id));
+    } else {
+      dispatchToPosts(starredPost(id));
+    }
+  };
+
+  return (
+    <CustomContainer>
+      <Row>
+        <Col>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+          <Row>
+            <Col>
+              {" "}
+              <Link to={`/post/${post.id}/comments`}>View comments</Link>
+            </Col>
+            <Col>
+              {showStarred && (
+                <Button onClick={handleStarred} color="info">
+                  {post.isStarred ? "Remove from Fav." : "Add To Fav."}
+                </Button>
+              )}
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </CustomContainer>
+  );
+};
---
+++ src/components/PostList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+import { Post } from "./Post";
+
+const Container = styled.div`
+  margin-top: 12px;
+`;
+export const PostList = ({ posts, allowStarredPost }) => {
+  return (
+    <Container>
+      {posts.map(post => (
+        <div key={post}>
+          <Post id={post} showStarred={allowStarredPost} />
+          <hr />
+        </div>
+      ))}
+    </Container>
+  );
+};
