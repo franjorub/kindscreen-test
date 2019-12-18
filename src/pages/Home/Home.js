0738dfca54055b19ffe16f3538ef6e55870a4c29
Francisco Rubial <franjorub@gmail.com>
2019-12-18 00:12:32 -0400

    display tab for posts feed and favorites

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,29 +1,53 @@
-import React, { useState, useEffect } from "react";
-import { Button, Row, Col } from "reactstrap";
+import React, { useState, useEffect, useMemo } from "react";
+import {
+  Button,
+  Row,
+  Col,
+  TabContent,
+  TabPane,
+  Nav,
+  NavItem,
+  NavLink
+} from "reactstrap";
+import classnames from "classnames";
 import { Header } from "./styles";
+import { PostList } from "../../components/PostList";
 import { useAuth } from "../../contexts/auth";
 import client from "../../api/client";
-import { Post } from "../../components/Post";
+import { usePosts } from "../../contexts/posts";
+import { fetchPostSuccess, loadingPosts } from "../../actions/posts";
 
 export const Home = () => {
-  const [posts, setPosts] = useState([]);
+  const [activeTab, setActiveTab] = useState("1");
   const { loggedUser, authUser } = useAuth();
+  const [posts, dispatchToPosts] = usePosts();
+
+  const toggle = tab => {
+    if (activeTab !== tab) setActiveTab(tab);
+  };
 
   const handleLoggout = () => {
     authUser(null);
   };
 
+  const starredPosts = useMemo(
+    () => posts.keys.filter(key => posts.entities[key].isStarred),
+    [posts]
+  );
+
   useEffect(() => {
     const getPosts = async () => {
       try {
+        dispatchToPosts(loadingPosts());
         const posts = await client.getPostsByUserId(loggedUser.id);
-        setPosts(posts);
+
+        dispatchToPosts(fetchPostSuccess(posts));
       } catch (err) {
         console.log(err);
       }
     };
     getPosts();
-  }, [loggedUser.id]);
+  }, [loggedUser.id, dispatchToPosts]);
 
   return (
     <div>
@@ -39,9 +63,40 @@ export const Home = () => {
           </Col>
         </Row>
       </Header>
-      {posts.map(post => (
-        <Post post={post} key={post.id} />
-      ))}
+      <Nav tabs>
+        <NavItem>
+          <NavLink
+            className={classnames({ active: activeTab === "1" })}
+            onClick={() => {
+              toggle("1");
+            }}
+          >
+            Posts Feed
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink
+            className={classnames({ active: activeTab === "2" })}
+            onClick={() => {
+              toggle("2");
+            }}
+          >
+            Favorite Posts
+          </NavLink>
+        </NavItem>
+      </Nav>
+      <TabContent activeTab={activeTab}>
+        <TabPane tabId="1">
+          {posts.loading ? (
+            <p>loading your posts</p>
+          ) : (
+            <PostList posts={posts.keys} allowStarredPost />
+          )}
+        </TabPane>
+        <TabPane tabId="2">
+          <PostList posts={starredPosts} />
+        </TabPane>
+      </TabContent>
     </div>
   );
 };
