418861d5d15f38b6bb27b24e5723d83540db3659
Francisco Rubial <franjorub@gmail.com>
2019-12-18 00:14:21 -0400

    connected post context to reducer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 import { Comments } from "./pages/Comments";
-import { AuthContext } from "./contexts/auth";
 import { PrivateRoute } from "./components/PrivateRoute";
+import { AuthContext } from "./contexts/auth";
+import { PostsContext } from "./contexts/posts";
+import { initialPostState, postReducer } from "./reducers/posts";
 
 const App = () => {
   const [loggedUser, setLoggedUser] = useState(null);
+  const [posts, dispatchToPosts] = useReducer(postReducer, initialPostState);
 
   useEffect(() => {
     const checkLocalSession = async () => {
@@ -28,11 +31,13 @@ const App = () => {
 
   return (
     <AuthContext.Provider value={{ loggedUser, authUser }}>
-      <Router>
-        <Route exact path="/" component={Login} />
-        <PrivateRoute path="/welcome" component={Home} />
-        <PrivateRoute path="/post/:id/comments" component={Comments} />
-      </Router>
+      <PostsContext.Provider value={[posts, dispatchToPosts]}>
+        <Router>
+          <Route exact path="/" component={Login} />
+          <PrivateRoute path="/welcome" component={Home} />
+          <PrivateRoute path="/post/:id/comments" component={Comments} />
+        </Router>
+      </PostsContext.Provider>
     </AuthContext.Provider>
   );
 };
