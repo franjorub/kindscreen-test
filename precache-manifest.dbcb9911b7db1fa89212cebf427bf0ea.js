07c27095c10ff11e3da94507cf9c2cc4d0a6b113
Francisco Rubial <franjorub@gmail.com>
2019-12-18 00:59:40 -0400

    Updates

---
+++ precache-manifest.dbcb9911b7db1fa89212cebf427bf0ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b6dcb7657cee557669321f9f889023b",
+    "url": "/kindscreen-test/index.html"
+  },
+  {
+    "revision": "3339f55ada6618d93fa6",
+    "url": "/kindscreen-test/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "3339f55ada6618d93fa6",
+    "url": "/kindscreen-test/static/js/2.b051df95.chunk.js"
+  },
+  {
+    "revision": "b651d5533fc80509587379dd07cc06bd",
+    "url": "/kindscreen-test/static/js/2.b051df95.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4615284a47794f52b11b",
+    "url": "/kindscreen-test/static/js/main.1c12d366.chunk.js"
+  },
+  {
+    "revision": "b7d5a595c407edc48ca1",
+    "url": "/kindscreen-test/static/js/runtime-main.91b4c502.js"
+  }
+]);
